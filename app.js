c49e2420ed45ee69cd6c67d9c418ea9389655b79
itsyaboimacoy <61514749+itsyaboimacoy@users.noreply.github.com>
2022-12-18 02:02:14 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
-$('.center').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+let hamberger = document.querySelector('.hamberger-button');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+
+hamberger.addEventListener('click',function(){
+  mobileNav.classList.add('open');
+});
+
+times.addEventListener('click',function(){
+  mobileNav.classList.remove('open');
+});
